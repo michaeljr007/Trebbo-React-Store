8b2c22b32e9d7f891f3fc78edf7e2935371317e2
Michael Isih <raregoldmichael98@gmail.com>
2023-09-15 09:45:48 +0100

    Add files via upload

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../Components/Navbar";
+import Main from "../Components/Main";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Main />
+    </>
+  );
+};
+
+export default Home;
