2e86c0f4f58f4ea8102f6a204725c74169dbeea4
Michael Isih <raregoldmichael98@gmail.com>
2023-09-15 09:44:49 +0100

    Delete src directory

--- src/redux/Store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import ProductReducer from "./slices/ProductSlice";
-
-export const Store = configureStore({
-  reducer: {
-    Products: ProductReducer,
-  },
-});
